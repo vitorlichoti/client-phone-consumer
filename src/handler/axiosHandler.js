572edcb2c82eaa3fdbe075e5bc04a9a3fec0a499
Vitor <vitorlichoti@gmail.com>
2024-03-09 10:52:41 -0400

    substitui url hardcoded por env

--- src/handler/axiosHandler.js
+++ src/handler/axiosHandler.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const axiosAuthHandler = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.AUTH_API,
   timeout: 1000
 })
 
 const axiosProductsHandler = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.RESTFULL_API,
   timeout: 10000
 })
 
